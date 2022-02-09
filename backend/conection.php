e204e2cbf6b5cfdd375eae4b623844ac16bd3373
fguedez <fguedez1311@gmail.com>
2022-02-09 14:44:33 -0400

    La camara captura  la imagen perfectamente y la guarda a la base de datos

--- backend/conection.php
+++ backend/conection.php
@@ -3,7 +3,7 @@
 		$hostname = "localhost";
 		$dbname = "camaraweb";
 		$username = "postgres";
-		$pw = "123456";
+		$pw = "admin";
 		$db = new PDO('pgsql:host='.$hostname.';dbname='.$dbname.'', $username, $pw);
 	} catch (PDOException $ex) {
 		echo "Error al conectar a la base de datos: " . $ex->getMessage() . "\n";
